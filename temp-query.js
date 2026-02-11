c188805e415464f548159527c8fceb8aaa3a3af2
marcelodestcert <marcelo@destcert.com>
2026-02-11 20:50:54 -0300

    oh meu deus

--- temp-query.js
+++ temp-query.js
@@ -1,4 +1,4 @@
-﻿const { PrismaClient } = require('./prisma/generated/client');
+﻿const { PrismaClient } = require('@prisma/client');
 const p = new PrismaClient();
 (async () => {
   const tasks = await p.task.findMany({ where: { boardId: 'restore-prod-board' }, select: { id: true, title: true, boardId: true }, take: 5 });
